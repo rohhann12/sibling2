3859ce25cea262c3918fe7ae8b67f0280b48818c
Rohan <rsharma7_be22@thapar.edu>
2024-07-02 02:32:38 +0530

    init

--- src/components/LandingPage/contactUs/ContactUs.jsx
+++ src/components/LandingPage/contactUs/ContactUs.jsx
@@ -1,18 +1,18 @@
-import './ContactUs.css'
-import React, { useState } from 'react'
-import axios from 'axios'
-import Left_img from "../../assets/contact/Group 2043684748.svg"
-const EmailRestAPI = () => {
+import './ContactUs.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Left_img from '../../assets/contact/Group 2043684748.svg';
 
+const EmailRestAPI = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-const serviceId = import.meta.env.VITE_SERVICE_ID;
-const templateId = import.meta.env.VITE_TEMPLATE_ID;
-const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+    const serviceId = import.meta.env.VITE_SERVICE_ID;
+    const templateId = import.meta.env.VITE_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_PUBLIC_KEY;
 
     const data = {
       service_id: serviceId,
@@ -23,63 +23,58 @@ const publicKey = import.meta.env.VITE_PUBLIC_KEY;
         from_email: email,
         to_name: 'Viney',
         message: message,
-      }
+      },
     };
 
-    // Send the email using EmailJS
     try {
-      const res = await axios.post("https://api.emailjs.com/api/v1.0/email/send", data);
+      const res = await axios.post('https://api.emailjs.com/api/v1.0/email/send', data);
       console.log(res.data);
       setName('');
       setEmail('');
       setMessage('');
-      alert("Your message has been sent")
+      alert('Your message has been sent');
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
-   <>
     <div className="contact-us-container">
-      
       <div className="img-on-left">
-        <img src={Left_img} alt="Contact Us" className='map' />
+        <img src={Left_img} alt="Contact Us" className="map" />
       </div>
       <div className="form-on-right">
-        <h2 className='heading3 font-bold'>Contact Us</h2>
-        <p className='font-semibold'>hqsibling@gmail.com</p>
-
+        <h2 className="heading3 font-bold">Contact Us</h2>
+        <p className="font-semibold">hqsibling@gmail.com</p>
         <form onSubmit={handleSubmit}>
-          <label className='font-bold'>Full Name</label>
-          <input 
-            type="text" 
+          <label className="font-bold">Full Name</label>
+          <input
+            type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder="Wahiq Iqbal" 
-            required={true}
-            
+            placeholder="Wahiq Iqbal"
+            required
           />
-          <label className='font-bold'>Email</label>
-          <input 
-            type="email" 
+          <label className="font-bold">Email</label>
+          <input
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="example@gmail.com" 
-            required 
+            placeholder="example@gmail.com"
+            required
           />
-          <label className='font-bold'>Your Message</label>
-          <textarea 
+          <label className="font-bold">Your Message</label>
+          <textarea
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            placeholder="Enter your message here!" 
-            required 
+            placeholder="Enter your message here!"
+            required
           />
-          <button type="submit" className='font-bold'>Send Message</button>
+          <button type="submit" className="font-bold">Send Message</button>
         </form>
       </div>
     </div>
-   </>
-  )
-}
+  );
+};
+
 export default EmailRestAPI;
