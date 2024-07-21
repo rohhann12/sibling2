844123f7bfb9d05df0065fa1145c7f7dd259a2b9
Rohan Sharma <rsharma7_be22@thapar.edu>
2024-07-21 17:47:59 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Newsletter from './components/LandingPage/Newsletter/Main-2';
 import LandingPage from './components/LandingPage/LandingPage';
 import ComingSoon from './components/Navbar/ComingSoon';
-// import Pdf from './components/LandingPage/Newsletter/rendering-pdf/pdf'
-// import View from './components/LandingPage/section3/section3'
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/newsletter" exact={false} element={<Newsletter />} />
-          {/* <Route path="/newsletter/sample" exact={false} element={<Pdf />} /> */}
           <Route path='/space' exact={false}  element={<ComingSoon />} />
           <Route path="*" element={<h1>Route does not exist</h1>} />
         </Routes>
