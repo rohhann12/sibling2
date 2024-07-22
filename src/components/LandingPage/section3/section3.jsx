5f138d10146fc70e6796b3aa6b45b855188faed5
Rohan Sharma <rsharma7_be22@thapar.edu>
2024-07-23 04:38:36 +0530

    sec3

--- src/components/LandingPage/section3/section3.jsx
+++ src/components/LandingPage/section3/section3.jsx
@@ -7,6 +7,7 @@ import "../../../fonts/Manrope/static/Manrope-Regular.ttf"
 import "../../../fonts/Montserrat/static/Montserrat-Regular.ttf"
 import "../../../fonts/Montserrat/static/Montserrat-Medium.ttf"
 import "../../../fonts/Montserrat/static/Montserrat-SemiBold.ttf"
+
 const VerticalCardSlider = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -32,32 +33,31 @@ const VerticalCardSlider = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setActiveIndex((prevIndex) => (prevIndex + 1) % contentArray.length);
-    }, 3000); 
+    }, 3000);
 
     return () => clearInterval(interval);
   }, [contentArray.length]);
 
   return (
     <>
-    <p className='heading_help'>HOW CAN WE HELP</p>
-    <div className="wrapper">
-      <div className="outer">
-        <div className="titles">
-          {contentArray.map((item, index) => (
-            <h2
-              key={index}
-              className={`name ${index === activeIndex ? 'active' : ''}`}
-            >
-              {item.title}
-            </h2>
-          ))}
-        </div>
-        <div className="description">
-          <p>{contentArray[activeIndex].description}</p>
-          <p className='static'>We focus on nurturing all aspects of your well-being, including mental, emotional, and physical health, ensuring you grow into a well-rounded individual.</p>
+      <p className='heading_help'>HOW CAN WE HELP</p>
+      <div className="wrapper">
+        <div className="outer">
+          <div className="titles">
+            {contentArray.map((item, index) => (
+              <h2
+                key={index}
+                className={`name ${index === activeIndex ? 'active' : ''}`}
+              >
+                {item.title}
+              </h2>
+            ))}
+          </div>
+          <div className="description">
+            <p>{contentArray[activeIndex].description}</p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
