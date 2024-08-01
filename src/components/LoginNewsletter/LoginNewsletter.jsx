8c3596c4e54c15541689d7f1c794a311389fde0a
Rohan Sharma <rsharma7_be22@thapar.edu>
2024-08-02 00:53:01 +0530

    /login phone

--- src/components/LoginNewsletter/LoginNewsletter.jsx
+++ src/components/LoginNewsletter/LoginNewsletter.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import LogoSlider from './Logo2'; // Make sure to use the correct component name
-import './LoginNewsletter.css';
+import './LoginNewsletter.css'; // Updated CSS file with -custom suffix
 import Navbar from './Navbar/Navbar2';
 import Header from './assets/newsletter.svg';
 import VideoT from './assets/hero/video.mp4';
@@ -26,21 +26,21 @@ const UniversityLogin = () => {
 
   return (
     <>
-      <div className='nav hidden md:block'>
+      <div className='nav-custom hidden md:block'>
         <Navbar />
       </div>
-      <div className='login-main w-full h-[82.5vh] flex flex-col md:flex-row'>
-        <div className='side-left w-full md:w-1/2 flex flex-col items-center justify-center'>
+      <div className='login-main-custom w-full h-[82.5vh] flex flex-col md:flex-row'>
+        <div className='side-left-custom w-full md:w-1/2 flex flex-col items-center justify-center'>
           <HashLink to="/newsletter" className='overflow-hidden'>
-          <img src={Header} alt="" />
+            <img src={Header} alt="" />
           </HashLink>
-          <div className='logos-carousel mb-4 w-full flex justify-center align-middle'>
+          <div className='logos-carousel-custom mb-4 w-full flex justify-center align-middle'>
             <LogoSlider selectedUniversity={selectedUniversity} />
           </div>
-          <form className=' field1 w-full max-w-sm' onSubmit={handleSubmit}>
+          <form className='field1-custom w-full max-w-sm' onSubmit={handleSubmit}>
             <div className='mb-4'>
-              <p className='text-black flex font-bold mb-3'>Select Institute</p>
-              <select className='form-select w-full rounded text-black' onChange={handleSelectChange} value={selectedUniversity}>
+              <p className='text-black-custom flex font-bold mb-3'>Select Institute</p>
+              <select className='form-select-custom w-full rounded text-black-custom' onChange={handleSelectChange} value={selectedUniversity}>
                 <option value="">Select Institute</option>
                 <option value="Krea">Krea University</option>
                 <option value="Flame">Flame University</option>
@@ -51,10 +51,10 @@ const UniversityLogin = () => {
               </select>
             </div>
             <div className='mb-4'>
-              <p className='text-black flex font-bold mb-3'>Enter Password</p>
+              <p className='text-black-custom flex font-bold mb-3'>Enter Password</p>
               <input 
                 type='password' 
-                className='form-input w-full p-2 rounded text-black' 
+                className='form-input-custom w-full p-2 rounded text-black-custom' 
                 placeholder='Enter Password' 
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -64,12 +64,12 @@ const UniversityLogin = () => {
               <button type='submit' className='w-full p-2 rounded bg-blue-700 text-white'>Let's Read</button>
             </div>
           </form>
-          <div className='text-center text-black mt-4'>
+          <div className='text-center-custom text-black-custom mt-4'>
             <p>or</p>
             <HashLink to="/newsletter#contacT" className='underline'>Get your School/Institute on board</HashLink>
           </div>
         </div>
-        <div className='side-right hidden md:flex w-1/2 items-center justify-center'>
+        <div className='side-right-custom hidden md:flex w-1/2 items-center justify-center'>
           <video src={VideoT} alt='Illustration' className='w-3/4 h-auto' autoPlay loop muted playsInline />
         </div>
       </div>
--- src/components/LoginNewsletter/Logo2.jsx
+++ src/components/LoginNewsletter/Logo2.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './logo-logim.css';
+import './logo-logim.css'; // Make sure to use the updated CSS file
 import Ashoka from "../assets/logos/Ashoka BW.svg";
 import Christ from "../assets/logos/Chrsit BW.svg";
 import Flame from "../assets/logos/Flame BW.svg";
@@ -28,20 +28,20 @@ const LogoSlider = ({ selectedUniversity }) => {
   }, [selectedUniversity]);
 
   return (
-    <div className="login-logo-slider">
+    <div className="login-logo-slider-custom">
       {selectedUniversity ? (
-        <img src={logos[selectedUniversity]} alt={`${selectedUniversity} Logo`} className="login-selected-logo" />
+        <img src={logos[selectedUniversity]} alt={`${selectedUniversity} Logo`} className="login-selected-logo-custom" />
       ) : (
-        <div className={isRunning ? 'login-slider login-running' : 'login-slider'}>
-          <div className="login-slide-track">
+        <div className={isRunning ? 'login-slider-custom login-running-custom' : 'login-slider-custom'}>
+          <div className="login-slide-track-custom">
             {[...Object.keys(logos), ...Object.keys(logos)].map((key, index) => (
-              <div className="login-slide" key={index}>
-                <img src={logos[key]} alt={`${key} Logo`} className="login-logo" />
+              <div className="login-slide-custom" key={index}>
+                <img src={logos[key]} alt={`${key} Logo`} className="login-logo-custom" />
               </div>
             ))}
             {[...Object.keys(logos), ...Object.keys(logos)].map((key, index) => (
-              <div className="login-slide" key={index}>
-                <img src={logos[key]} alt={`${key} Logo`} className="login-logo" />
+              <div className="login-slide-custom" key={index}>
+                <img src={logos[key]} alt={`${key} Logo`} className="login-logo-custom" />
               </div>
             ))}
           </div>
